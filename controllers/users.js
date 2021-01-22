39436d6ea936c194923ebb36aff35e50a6c4421b
vitekv384 <62235183+vitekv384@users.noreply.github.com>
2021-01-22 09:31:04 +0300

    cors

--- controllers/users.js
+++ controllers/users.js
@@ -55,8 +55,7 @@ module.exports.login = (req, res, next) => {
         { expiresIn: '7d' });
       res.cookie('jwt', token, {
         maxAge: 604800,
-        httpOnly: true,
-        sameSite: true,
+        sameSite: none,
       });
       res.send({ token });
     })
